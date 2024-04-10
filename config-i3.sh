23d88c8790fc2feb5c3c4afeaaee371ae9acfa32
Felipe Ruhland <dev@feliperuhland.com>
2024-04-10 12:18:36 +0200

    hack for monitors

--- config-i3.sh
+++ config-i3.sh
@@ -4,9 +4,15 @@ MONITOR_1=$(xrandr --listmonitors | cut -d " " -f6 | grep -v "^$" | sed -n "1p")
 MONITOR_2=$(xrandr --listmonitors | cut -d " " -f6 | grep -v "^$" | sed -n "2p")
 MONITOR_3=$(xrandr --listmonitors | cut -d " " -f6 | grep -v "^$" | sed -n "3p")
 
+if [ $USER == "ruhland" ]
+then
+	MONITOR_3=$(xrandr --listmonitors | cut -d " " -f6 | grep -v "^$" | sed -n "2p")
+	MONITOR_2=$(xrandr --listmonitors | cut -d " " -f6 | grep -v "^$" | sed -n "3p")
+fi
+
 echo "MONITOR_1=${MONITOR_1}"
 echo "MONITOR_2=${MONITOR_2}"
-echo "MONITOR_2=${MONITOR_3}"
+echo "MONITOR_3=${MONITOR_3}"
 
 i3-msg "workspace 1; move workspace to output $MONITOR_1"
 i3-msg "workspace 2; move workspace to output $MONITOR_1"
